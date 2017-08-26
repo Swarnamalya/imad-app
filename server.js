74b74f2df55e56a134e8d508e20f33276b23196c
Swarnamalya Balaji <sbbswarna4@gmail.com>
2017-08-27 00:23:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,7 +4,83 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
+var articles =
+{
+'article-one' : {
+    title: 'Article one by Swarnamalya',
+    heading: 'Article One',
+    date: 'Aug 15, 2017',
+    content: `<p>
+                This is first line of Article One.This is interesting. Created for imad course.
+                his is first line of Article One.This is interesting. Created for imad course.
+                his is first line of Article One.This is interesting. Created for imad course.
+                his is first line of Article One.This is interesting. Created for imad course.
+            </p>
+            <p>
+                his is first line of Article One.This is interesting. Created for imad course.his is first line of Article One.This is interesting. Created for imad course.his is first line of Article One.This is interesting. Created for imad course.
+            his is first line of Article One.This is interesting. Created for imad course.
+            </p>
+            <p>
+                his is first line of Article One.This is interesting. Created for imad course.
+                his is first line of Article One.This is interesting. Created for imad course.
+                his is first line of Article One.This is interesting. Created for imad course.
+                his is first line of Article One.This is interesting. Created for imad course.
+            </p> `
+    
+    
+},
+'article-two' :{
+     title: 'Article two by Swarnamalya',
+    heading: 'Article One',
+    date: 'Aug 8, 2017',
+    content: `<p>
+                This is first line of Article two.This is interesting. Created for imad course.</p>`
+},
+'article-three' : {
+     title: 'Article three by Swarnamalya',
+    heading: 'Article Three',
+    date: 'Aug 27, 2017',
+    content: `<p>
+                This is first line of Article Three.This is interesting. Created for imad course.</p>`
+}
+};
 
+
+function createTemplate(data)
+{
+    var title=data.title;
+    var date=data.date;
+    var heading=data.heading;
+    var content=data.content;
+var htmlTemplate =
+` <html>
+    <head>
+        <title> ${title} </title>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <link href="/ui/style.css" rel="stylesheet" />
+    
+    </head>
+    <body>
+        <div class="container">
+        <div>
+            <a href="/">home</a>
+        </div>
+        <hr/>
+        <h3>${heading}</h3>
+        <div>
+            ${date}
+        </div>
+        <div>
+            ${content}
+        </div>
+        </div>
+        
+    </body>
+    
+</html>
+`;
+return htmlTemplate;
+}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -16,15 +92,11 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
-app.get('/article-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+app.get('/articleName', function (req, res) {
+    var articleName= req.params.articleName;
+  res.send(createTemplate(articles[articleName]));
 });
-app.get('/article-two', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-});
-app.get('/article-three', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
-});
+
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
