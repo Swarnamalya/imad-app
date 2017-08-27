8fb1262ec05985c5ed1faf48bc77cf0418516472
Swarnamalya Balaji <sbbswarna4@gmail.com>
2017-08-27 20:01:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,14 @@ console.log('Loaded!');
 var element=document.getElementById('main-text');
 element.innerHTML='New Value';
 var img = document.getElementById('madi');
+var marginLeft=0;
+function moveRight()
+{
+    marginLeft=marginLeft+10;
+    img.style.marginLeft=marginLeft+'px';
+}
 img.onclick = function()
 {
-    img.style.marginLeft='100px';
+    //img.style.marginLeft='100px';
+    var interval = setInterval(moveRight, 100);
 };
